256c1dc6a09eb6473226565ac828c39f644da1c5
jaydip dodariya <jadipdodariya2002@gmail.com>
2025-09-17 14:19:33 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,188 @@
+// First - Section - Slider ------
+$('.first-section .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        641: {
+            items: 1,
+        },
+        1000: {
+            items: 1,
+        }
+    }
+});
+
+
+// Four - Section - Slider ------
+$('.four-section .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1,
+            margin: 20,
+        },
+        640: {
+            items: 2,
+            margin: 20,
+        },
+        1025: {
+            items: 3,
+        }
+    }
+})
+
+
+
+// Five - Section - Slider ------
+$('.five-section .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        980: {
+            items: 3,
+        }
+    }
+});
+
+
+// Six - Section - Slider ------
+$('.six-section .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1,
+            margin: 20,
+        },
+        640: {
+            items: 2,
+            margin: 20,
+        },
+        980: {
+            items: 3,
+        }
+    }
+})
+
+
+// Seven - Section - Slider------
+$('.seven-section .owl-carousel').owlCarousel({
+    items: 2,
+    loop: true,
+    margin: 30,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            margin: 20,
+        },
+        890: {
+            items: 2,
+            margin: 20,
+        },
+        1000: {
+            items: 2,
+        }
+    }
+});
+
+
+// // Eight - Section - Slider ------
+$('.eight-section .owl-carousel').owlCarousel({
+    items: 3,
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            margin: 20,
+            items: 1,
+        },
+        640: {
+            margin: 20,
+            items: 2,
+
+        },
+        1000: {
+            items: 3,
+        }
+    }
+});
+
+
+// Mobile - Menu --------------------
+document.addEventListener(
+    "DOMContentLoaded", () => {
+        new Mmenu("#menu", {
+            "offCanvas": {
+                "position": "right-front"
+            },
+            "theme": "white",
+            "iconbar": {
+                "use": true,
+                "top": [
+                    "<a href='#/'><i class='fa fa-home'></i></a>"
+                ],
+                "bottom": [
+                    "<a href='#/'><i class='fa-solid fa-envelope'></i></a>",
+                    "<a href='#/'><i class='fa-solid fa-arrow-right-to-bracket'></i></a>",
+                    "<a href='#/'><i class='fa-solid fa-user'></i></a>"
+                ]
+            },
+            "navbars": [
+                {
+                    "position": "top",
+                    "content": [
+                        "searchfield"
+                    ]
+                },
+                {
+                    "position": "top",
+                    "content": [
+                        "prev",
+                        "title"
+                    ]
+                }
+            ]
+        });
+    }
+);
+
+
+$(document).ready(function () {
+
+    $('.search a .fa-magnifying-glass').click(function () {
+        $('.input-fild').fadeIn();
+        $('.search').addClass('active');
+    })
+
+    $('.search a .fa-xmark').click(function () {
+        $('.input-fild').fadeOut();
+        $('.search').removeClass('active');
+    })
+
+})
